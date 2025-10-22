2e8f60e40f5fa8412b38f06a5bd5d5a2bf5d9886
SamanthaPPW <samanthappw@gmail.com>
2025-10-22 08:22:11 +0200

    base des bases, sans front fonctionnel

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>SEM</title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+  <header>
+    <img src="images/logo.svg" alt="Logo de SEM" class="logo">
+    <nav>
+      <ul>
+        <li><a href="index.php">Accueil</a></li>
+        <li><a href="explorer.php">Explorer la Carte</a></li>
+        <li><a href="poi.php">Que faire en arrivant ?</a></li>
+      </ul>
+    </nav>
+        
+  </header>
+  <div id="carte">
+<iframe title="Cartes des offres étudiantes par région" aria-label="Carte choroplèthe" id="datawrapper-chart-LWMtH" src="https://datawrapper.dwcdn.net/LWMtH/2/" scrolling="no" frameborder="0" data-external="1"></iframe><script type="text/javascript">window.addEventListener("message",function(a){if(void 0!==a.data["datawrapper-height"]){var e=document.querySelectorAll("iframe");for(var t in a.data["datawrapper-height"])for(var r,i=0;r=e[i];i++)if(r.contentWindow===a.source){var d=a.data["datawrapper-height"][t]+"px";r.style.height=d}}});</script>
+    </div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+<!--Une page qui se concentre sur l'arrivée. Elle répond à la question : "Est-ce que j'ai besoin d'une voiture ?".
+On y voit une mini-carte avec les Points d'Intérêt (POI) pertinents pour les étudiants (auberges, activités, restos) et les options de mobilité douce (vélo, bus) à proximité de la gare.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<img src="https://i.pinimg.com/736x/d6/a8/cd/d6a8cda7b9adfa8138840c423ebcebaa.jpg" style="width:1600px"> 
