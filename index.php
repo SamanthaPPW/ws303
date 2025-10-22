f839ca01e04f7338c49959dd50dd45f7f7b80714
SamanthaPPW <samanthappw@gmail.com>
2025-10-22 12:17:33 +0200

    Header integré

--- index.php
+++ index.php
@@ -1,59 +1,39 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>SEM</title>
   <link rel="stylesheet" href="css/styles.css">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Smooch+Sans:wght@100..900&display=swap" rel="stylesheet">
 </head>
+
 <body>
   <header>
-    <img src="images/logo.svg" alt="Logo de SEM" class="logo">
+    <img src="images/logo.svg" alt="Logo SEM" class="logo">
+
     <nav>
       <ul>
-        <li><a href="index.php">Accueil</a></li>
-        <li><a href="explorer.php">Explorer la Carte</a></li>
-        <li><a href="poi.php">Que faire en arrivant ?</a></li>
+        <li><a href="index.php" class="actuel">Accueil</a></li>
+        <li><a href="explorer.php">Explorer</a></li>
+        <li><a href="apropos.php">A propos</a></li>
+        <li><a href="contact.php">Contact</a></li>
       </ul>
     </nav>
-        
+    <div class="header-spacer"></div>
   </header>
   <div id="carte">
-<iframe title="Cartes des offres étudiantes par région" aria-label="Carte choroplèthe" id="datawrapper-chart-LWMtH" src="https://datawrapper.dwcdn.net/LWMtH/2/" scrolling="no" frameborder="0" data-external="1"></iframe><script type="text/javascript">window.addEventListener("message",function(a){if(void 0!==a.data["datawrapper-height"]){var e=document.querySelectorAll("iframe");for(var t in a.data["datawrapper-height"])for(var r,i=0;r=e[i];i++)if(r.contentWindow===a.source){var d=a.data["datawrapper-height"][t]+"px";r.style.height=d}}});</script>
-    </div>
+    <iframe title="Cartes des offres étudiantes par région" aria-label="Carte choroplèthe" id="datawrapper-chart-LWMtH"
+      src="https://datawrapper.dwcdn.net/LWMtH/2/" scrolling="no" frameborder="0" data-external="1"></iframe>
+    <script
+      type="text/javascript">window.addEventListener("message", function (a) { if (void 0 !== a.data["datawrapper-height"]) { var e = document.querySelectorAll("iframe"); for (var t in a.data["datawrapper-height"]) for (var r, i = 0; r = e[i]; i++)if (r.contentWindow === a.source) { var d = a.data["datawrapper-height"][t] + "px"; r.style.height = d } } });</script>
+  </div>
+
+  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d10621.44664175462!2d4.079616000000001!3d48.2766577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sfr!4v1761126827003!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </body>
-</html>
 
-
-
-
-
-
-
-
-<!--Une page qui se concentre sur l'arrivée. Elle répond à la question : "Est-ce que j'ai besoin d'une voiture ?".
-On y voit une mini-carte avec les Points d'Intérêt (POI) pertinents pour les étudiants (auberges, activités, restos) et les options de mobilité douce (vélo, bus) à proximité de la gare.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<img src="https://i.pinimg.com/736x/d6/a8/cd/d6a8cda7b9adfa8138840c423ebcebaa.jpg" style="width:1600px"> 
+</html>   
